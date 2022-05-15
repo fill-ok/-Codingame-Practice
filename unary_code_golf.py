fd82d138c6b58744a4045f8181be81977d50ec8f
fill <101579743+fill-ok@users.noreply.github.com>
2022-05-15 20:00:31 +0200

    Update unary_code_golf.py

--- unary_code_golf.py
+++ unary_code_golf.py
@@ -1,6 +1,7 @@
 # code golf // https://www.codingame.com/ide/puzzle/chuck-norris-codesize
 
 # code size = 125
+
 p=o=""
 for e in"".join(map(lambda l:f"{ord(l):07b}",[*input()])):
  if e!=p:o+=(" 00 "," 0 ")[int(e)];p=e
